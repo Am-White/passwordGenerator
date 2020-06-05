7ee18e68eed6a9aa3fc5692c0d2393c99a485863
Jamey Gronewald <jrgronewald@gmail.com>
2020-06-04 20:30:08 -0400

    transferred working code from sandbox.js to script.js; incorporated DOM elements by initializing userPreference function when generateBtn is clicked, and using innerHTML property of passwordText to display user's randomly generated password on the DOM; lot of code commented out-- still need to clean up script.js and ensure user stays within 8-128 character requirements and chooses at least one character set to include; also may try to simplify userPreference function by sourcing out the if statements to another function I can call with userPrefence()

--- script.js
+++ script.js
@@ -1,14 +1,105 @@
 // Assignment Code
 const generateBtn = document.querySelector("#generate");
+const passwordText = document.querySelector("#password");
+
+const lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+const uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const numerics = '0123456789';
+const specialCharacters = '!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~';
+const randomLowercaseLetter = lowercaseLetters[Math.floor(Math.random()*lowercaseLetters.length)];
+const randomUppercaseLetter = uppercaseLetters[Math.floor(Math.random()*uppercaseLetters.length)];
+const randomNumeric = numerics[Math.floor(Math.random()*numerics.length)];
+const randomSpecialCharacter = specialCharacters[Math.floor(Math.random()*specialCharacters.length)];
+
+
+// Initializing empty strings to use later
+let charSet = '';
+let finishedPassword = '';
+
+// Variables that need defining from user input on DOM elements and button push
+let includeLowercase
+let includeUppercase
+let includeNumeric
+let includeSpecial
+let passwordLength
 
 // Write password to the #password input
-const writePassword = function() {
-  let password = generatePassword();
-  const passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+/* const writePassword = function() {
+    let password = generatePassword();
+    const passwordText = document.querySelector("#password");
+    
+    passwordText.value = password;
 }
+*/
+/* Nested this function in userPreference for now
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function getCharSet() {
+    if (includeLowercase === true) {
+        finishedPassword += randomLowercaseLetter;
+        charSet += lowercaseLetters;
+        passwordLength -= 1;
+    }
+    if (includeUppercase === true) {
+        finishedPassword += randomUppercaseLetter;
+        charSet += uppercaseLetters;
+        passwordLength -= 1;
+    }
+    if (includeNumeric === true) {
+        finishedPassword += randomNumeric;
+        charSet += numerics;
+        passwordLength -= 1;
+    }
+    if (includeSpecial === true) {
+        finishedPassword += randomSpecialCharacter;
+        charSet+= specialCharacters;
+        passwordLength -= 1;
+    }
+    return charSet;
+    console.log(charSet);
+}; */
+
+// Function for user prompts-- nested function for getting charSet in userPreference for now
+const userPreference = function() {
+    alert('Welcome to password generator.');
+    includeLowercase = confirm('Would you like your password to contain lowercase letters?');
+    includeUppercase = confirm('Would you like your password to contain uppercase letters?');
+    includeNumeric = confirm('Would you like your password to include numeric characters?');
+    includeSpecial = confirm('Would you like your password to include special characters?');
+    passwordLength = prompt('How long would you like your password to be? Enter a number from 8 to 128.');
+    if (includeLowercase === true) {
+        finishedPassword += randomLowercaseLetter;
+        charSet += lowercaseLetters;
+        passwordLength -= 1;
+    }
+    if (includeUppercase === true) {
+        finishedPassword += randomUppercaseLetter;
+        charSet += uppercaseLetters;
+        passwordLength -= 1;
+    }
+    if (includeNumeric === true) {
+        finishedPassword += randomNumeric;
+        charSet += numerics;
+        passwordLength -= 1;
+    }
+    if (includeSpecial === true) {
+        finishedPassword += randomSpecialCharacter;
+        charSet+= specialCharacters;
+        passwordLength -= 1;
+    }
+    // Callback to generatePassword function defined below
+    generatePassword(charSet);
+};
+
+// Event listener on "Generate Password" button that initiates process of user selecting preferences for password
+generateBtn.addEventListener("click", userPreference);
+
+// Function for looping through possible
+const generatePassword = function(x) {
+    for(i = 0; i < passwordLength; i++) {
+        let passwordChar = x[Math.floor(Math.random()*x.length)];
+        finishedPassword += passwordChar;
+    };
+    passwordText.innerHTML = finishedPassword;
+};
+
+
